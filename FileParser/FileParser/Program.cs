1f4f55408a981bdff92395583df01046e64a2583
Makarova13 <44985075+Makarova13@users.noreply.github.com>
2019-09-01 21:51:29 +0300

    a

---
+++ FileParser/FileParser/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileParser
+{
+    class Program
+    {     
+        static void Main(string[] args)
+        {
+            string str = string.Empty;
+            string toSearch = string.Empty;
+            int counter = 0;
+
+            using (StreamReader netStream = new StreamReader("iugv")) 
+            {
+                netStream.Read(str.ToCharArray(), 0, 256);
+                counter += SearchString(str, toSearch).Length;
+            }
+        }
+
+        public static int[] SearchString(string str, string pat)
+        {
+            List<int> retVal = new List<int>();
+            int m = pat.Length;
+            int n = str.Length;
+
+            int[] badChar = new int[256];
+
+            BadCharHeuristic(pat, m, ref badChar);
+
+            int s = 0;
+            while (s <= (n - m))
+            {
+                int j = m - 1;
+
+                while (j >= 0 && pat[j] == str[s + j])
+                    --j;
+
+                if (j < 0)
+                {
+                    retVal.Add(s);
+                    s += (s + m < n) ? m - badChar[str[s + m]] : 1;
+                }
+                else
+                {
+                    s += Math.Max(1, j - badChar[str[s + j]]);
+                }
+            }
+
+            return retVal.ToArray();
+        }
+
+        private static void BadCharHeuristic(string str, int size, ref int[] badChar)
+        {
+            int i;
+
+            for (i = 0; i < 256; i++)
+                badChar[i] = -1;
+
+            for (i = 0; i < size; i++)
+                badChar[(int)str[i]] = i;
+        }
+    }
+}
