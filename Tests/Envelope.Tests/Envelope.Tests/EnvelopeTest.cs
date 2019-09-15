f3a9431a465f022b2a85be11ef9064d73ea91e02
Makarova13 <44985075+Makarova13@users.noreply.github.com>
2019-09-15 16:09:13 +0300

    vse

--- Tests/Envelope.Tests/Envelope.Tests/EnvelopeTest.cs
+++ Tests/Envelope.Tests/Envelope.Tests/EnvelopeTest.cs
@@ -9,54 +9,12 @@ namespace Task2Envelope.Tests
         [InlineData(3.1F, 7, 3.7F, 7.5F, Outcomes.FirstInSecond)]
         [InlineData(3.4F, 5.6F, 3.4F, 5.5F, Outcomes.None)]
         [InlineData(0.5F, 1.3F, 1.3F, 0.5F, Outcomes.Equal)]
-        public void TestPutIn(float a, float b, float c, float d, Outcomes expected)
+        public void TryPutIn_WithTwoEnvelopes_ShouldReturnExpectedOutcome(float a, float b, float c, float d, Outcomes expected)
         {
             Envelope en1 = new Envelope(a, b);
             Envelope en2 = new Envelope(c, d);
 
             Outcomes result = en1.CheckPutIn(en2);
-
-            Assert.True(result == expected);
-        }
-
-        [Theory]
-        [InlineData(3.4F, 5.501F, 3.4F, 5.501F, true)]
-        [InlineData(3.4F, 5.6F, 3.4F, 5.5F, false)]
-        [InlineData(0.5F, 1.3F, 1.3F, 0.5F, true)]
-        public void TestEquals(float a, float b, float c, float d, bool expected)
-        {
-            Envelope en1 = new Envelope(a, b);
-            Envelope en2 = new Envelope(c, d);
-
-            bool result = en1.Equals(en2);
-
-            Assert.True(result == expected);
-        }
-
-        [Theory]
-        [InlineData(1.1F, 3.501F, 3.4F, 5.501F, true)]
-        [InlineData(3.4F, 5.6F, 3.4F, 5.5F, false)]
-        [InlineData(0.5F, 0.3F, 1.3F, 2.5F, true)]
-        public void TestLess(float a, float b, float c, float d, bool expected)
-        {
-            Envelope en1 = new Envelope(a, b);
-            Envelope en2 = new Envelope(c, d);
-
-            bool result = en1 < en2;
-
-            Assert.True(result == expected);
-        }
-
-        [Theory]
-        [InlineData(1.1F, 3.501F, 3.4F, 5.501F, false)]
-        [InlineData(3.4F, 5.6F, 3.4F, 5.5F, false)]
-        [InlineData(0.5F, 0.3F, 1.3F, 2.5F, false)]
-        public void TestMore(float a, float b, float c, float d, bool expected)
-        {
-            Envelope en1 = new Envelope(a, b);
-            Envelope en2 = new Envelope(c, d);
-
-            bool result = en1 > en2;
 
             Assert.True(result == expected);
         }
